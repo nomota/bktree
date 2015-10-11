1282ca44d206314a73484eb012d0e6005d47649c
김형근 (Nomota Hiongun KIM) <hiongun@gmail.com>
2015-10-11 22:27:33 +0900

    Update bktree_test.go
    simplify the code

--- bktree_test.go
+++ bktree_test.go
@@ -1,109 +1,49 @@
-package bktree
+package main
 
 import (
-	"crypto/rand"
-	"encoding/base64"
-	"fmt"
-	"io"
-	"testing"
-	"time"
+    "fmt"
+    "./bktree"
+    "log"
+    "strings"
 )
 
-func TestLevenshtein(t *testing.T) {
-	s1, s2 := "hello", "world"
-	if Levenshtein(s1, s2) != 4 {
-		t.Fatal()
-	}
+func main() {
+    if bktree.Levenshtein("hello", "Aelo") != 2 {
+        log.Fatal()
+    }
 
-	if Levenshtein("", "AA") != 2 {
-		t.Fatal()
-	}
+    if bktree.Levenshtein("我爱你", "你爱我") != 2 {
+        log.Fatal()
+    }
 
-	if Levenshtein("a", "b") != 1 || Levenshtein("aa", "bb") != 2 {
-		t.Fatal()
-	}
+    bk := bktree.NewBKTreeNode("", 0)
 
-	if Levenshtein("abcd", "bcde") != 2 {
-		t.Fatal()
-	}
+    bk.Insert("ABCD")
+    bk.Insert("ACED")
+    bk.Insert("SBDE")
+    bk.Insert("가나다")
+    bk.Insert("가나다라")
+    bk.Insert("AABC")
+    bk.Insert("abcd")
+    bk.Insert("acd")
+    bk.Insert("go")
+    bk.Insert("went")
+    bk.Insert("love")
+    bk.Insert("bool")
+    bk.Insert("fact")
+    bk.Insert("join")
+    bk.Insert("eleven")
+    bk.Insert("seventeen")
+    bk.Insert("element")
+    bk.Insert("test")
+    
+    bk.Display()  // <----- Display in tree structured data
 
-	if Levenshtein("AB好C", "你好啊") != 3 {
-		t.Fatal()
-	}
+    fmt.Printf("bk.Size(): %d\n", bk.Size())  // <-- how many word is in this tree
 
-	if Levenshtein("你好世界啊", "世界啊你好") != 4 {
-		t.Fatal()
-	}
-}
+    ret := bk.Find("AABB", 2)
+    fmt.Println(ret)
 
-func TestInsert(t *testing.T) {
-	bk := New()
-	bk.SetLevenshteinLimit(2)
-
-	if !bk.Insert("") {
-		t.Fatal()
-	}
-
-	if bk.Insert("ABC") {
-		t.Fatal()
-	}
-
-	if !bk.Insert("AB") {
-		t.Fatal()
-	}
-}
-
-func TestInsertAndFind(t *testing.T) {
-	bk := New()
-	bk.SetLevenshteinLimit(50)
-
-	if bk.GetLevenshteinLimit() != 50 {
-		t.Fatal()
-	}
-
-	bk.Insert("656")
-	bk.Insert("67")
-	bk.Insert("9313")
-	bk.Insert("1178")
-	bk.Insert("38")
-
-	if bk.Size() != 5 {
-		t.Fatal()
-	}
-
-	ret := bk.Find("87", 2)
-	if ret[0] != "67" || ret[1] != "38" {
-		t.Fatal()
-	}
-
-	ret = bk.Find("87", 4)
-	if len(ret) != bk.Size() {
-		t.Fatal()
-	}
-}
-
-func Test(t *testing.T) {
-	bk := New()
-	bk.SetLevenshteinLimit(20)
-
-	start := time.Now()
-	for i := 0; i < 100000; i++ {
-		buf := make([]byte, 5)
-		io.ReadFull(rand.Reader, buf)
-		s := base64.StdEncoding.EncodeToString(buf)
-		bk.Insert(s)
-	}
-	fmt.Println("Insert", time.Since(start))
-
-	start = time.Now()
-	for i := 0; i < 10; i++ {
-		buf := make([]byte, 5)
-		io.ReadFull(rand.Reader, buf)
-		s := base64.StdEncoding.EncodeToString(buf)
-		ret := bk.Find(s, 5)
-		if len(ret) == 0 {
-
-		}
-	}
-	fmt.Println("Find", time.Since(start))
+    ret = bk.Find("joan", 1)
+    fmt.Println(ret)
 }
